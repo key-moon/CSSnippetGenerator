015f6fa4fb21f15c4860efc28ac6685eede16ea0
key-moon <kymn0116@gmail.com>
2019-09-23 16:42:18 +0900

    [add] linehandler class

---
+++ CSSnipetGenerator/Snippet/LineHandler/LineHandler.cs
@@ -0,0 +1,11 @@
+﻿
+
+partial class CodeSnippet
+{
+    private abstract class LineHandler
+    {
+        public CodeSnippet SnippetObject { get; private set; }
+        public LineHandler(CodeSnippet snippetObject) { SnippetObject = snippetObject; }
+        public abstract LineHandler NextLine(string line);
+    }
+}
