45dbccefb9897ed83a6420ddf42e799d51b485ea
key-moon <kymn0116@gmail.com>
2019-09-24 04:34:56 +0900

    [add] CodeHandler

---
+++ CSSnipetGenerator/Snippet/LineHandler/CodeHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+partial class CodeSnippet
+{
+    private class CodeHandler : LineHandler
+    {
+        IEnumerable<string> LiteralTokens;
+        StringBuilder codeBuilder = new StringBuilder();
+        public CodeHandler(CodeSnippet snippet) : this(snippet, Enumerable.Empty<string>()) { }
+        public CodeHandler(CodeSnippet snippet, IEnumerable<string> literalTokens) : base(snippet)
+        {
+            LiteralTokens = literalTokens;
+        }
+
+        public override LineHandler NextLine(string line)
+        {
+            codeBuilder.Append(line.Replace("$", "$$"));
+            return this;
+        }
+
+        public override void FinalizeSnippet()
+        {
+            var code = codeBuilder.ToString().Trim();
+            foreach (var token in LiteralTokens)
+                code = code.Replace(token, $"${token.TrimStart('@')}$");
+            SnippetObject.Snippet.Add(new CodeSnippetCode() { Language = "CSharp", Text = new string[] { code } });
+        }
+    }
+}
