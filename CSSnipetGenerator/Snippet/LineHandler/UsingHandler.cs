93567a15aae49377474a1b9b56a7793462e396ae
key-moon <kymn0116@gmail.com>
2019-09-23 17:24:06 +0900

    [add] using block handler

---
+++ CSSnipetGenerator/Snippet/LineHandler/UsingHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+partial class CodeSnippet
+{
+    const string DeclarationBeginToken = "#if !DECLARATIONS";
+    private class UsingHandler : LineHandler
+    {
+        List<string> Imports = new List<string>();
+
+        public UsingHandler(CodeSnippet snippet) : base(snippet) { }
+
+        public override LineHandler NextLine(string line)
+        {
+            if (!line.StartsWith("using"))
+            {
+                if (line == "") return this;
+                SnippetObject.Snippet.Add(new CodeSnippetImports() { Import = Imports.Select(x => new CodeSnippetImportsImport() { Namespace = x }).ToArray() });
+                if (line == DeclarationBeginToken) return new DeclarationHandler(SnippetObject).NextLine(line);
+                return new CodeHandler(SnippetObject).NextLine(line);
+            }
+            var import = line.Substring("using".Length).Trim(' ', ';');
+            Imports.Add(import);
+            return this;
+        }
+    }
+}
