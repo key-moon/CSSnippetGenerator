8684a30f0b7987703ddf6ca2ccc516d7e5b2620f
key-moon <kymn0116@gmail.com>
2019-09-23 16:08:30 +0900

    [add] parser

---
+++ CSSnipetGenerator/Snippet/Parser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+partial class CodeSnippet
+{
+    static CodeSnippet Parse(TextReader reader)
+    {
+
+    }
+}
