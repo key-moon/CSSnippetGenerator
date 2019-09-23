7dd9d87e6aa43e43cf73acd7e3198b9e8b374b8d
key-moon <kymn0116@gmail.com>
2019-09-23 12:23:10 +0900

    create project

---
+++ CSSnipetGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSSnipetGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
