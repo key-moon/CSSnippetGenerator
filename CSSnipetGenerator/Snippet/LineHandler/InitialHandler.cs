4df33f65838df498c8ef31a852a327c8c0680672
key-moon <kymn0116@gmail.com>
2019-09-28 12:42:26 +0900

    [add] format property

--- CSSnipetGenerator/Snippet/LineHandler/InitialHandler.cs
+++ CSSnipetGenerator/Snippet/LineHandler/InitialHandler.cs
@@ -3,7 +3,10 @@ partial class CodeSnippet
 {
     private class InitialHandler : LineHandler
     {
-        public InitialHandler(CodeSnippet snippetObject) : base(snippetObject) { }
+        public InitialHandler(CodeSnippet snippetObject) : base(snippetObject) 
+        {
+            SnippetObject.Format = "1.0.0";
+        }
         public override LineHandler NextLine(string line)
         {
             if (line.StartsWith("//")) return new HeaderHandler(SnippetObject).NextLine(line);
