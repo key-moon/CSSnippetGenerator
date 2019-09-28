1aba074804ae8e8bd9ef44cf8cdef9b913632a37
key-moon <kymn0116@gmail.com>
2019-09-28 17:35:49 +0900

    [change] ライブラリディレクトリをsecretから取得するのから引数から取得に変更

--- CSSnipetGenerator/Program.cs
+++ CSSnipetGenerator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Xml.Serialization;
 
 namespace CSSnipetGenerator
@@ -8,16 +9,25 @@ namespace CSSnipetGenerator
     {
         static void Main(string[] args)
         {
+            var libDir = args.Length == 0 ? GetLibraryDirectory() : args[0];
+            
             var snippets = new CodeSnippets();
-            foreach (var path in Directory.GetFiles(Path.Combine(Secrets.LibraryDirectory, "src"), "*.csx", SearchOption.AllDirectories))
+            foreach (var path in Directory.GetFiles(Path.Combine(libDir, "src"), "*.csx", SearchOption.AllDirectories))
             {
                 using StreamReader reader = new StreamReader(path);
                 var parsed = CodeSnippet.Parse(reader);
                 snippets.CodeSnippet.Add(parsed);
             }
+
             var serializer = new XmlSerializer(typeof(CodeSnippets));
-            using StreamWriter writer = new StreamWriter(Path.Combine(Secrets.LibraryDirectory, "snippets/snippet.snippet"));
+            using StreamWriter writer = new StreamWriter(Path.Combine(libDir, "snippets/snippet.snippet"));
             serializer.Serialize(writer, snippets);
+        }
+
+        static string GetLibraryDirectory()
+        {
+            Console.WriteLine("ライブラリのソースファイルが格納されているディレクトリを指定してください。");
+            return Console.ReadLine();
         }
     }
 }
